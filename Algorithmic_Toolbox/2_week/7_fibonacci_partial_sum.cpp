8feca5446ed5ff9c9eb9b1805a0940a824f53a0d
Shrirammc55 <shriram.mc.55@gmail.com>
2020-06-04 18:24:42 -0400

    7/8 completed - 8 in-progress

--- Algorithmic_Toolbox/2_week/7_fibonacci_partial_sum.cpp
+++ Algorithmic_Toolbox/2_week/7_fibonacci_partial_sum.cpp
@@ -10,20 +10,66 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
 
     for (long long i = 0; i <= to; ++i) {
         if (i >= from) {
-            sum += current%10;
+            sum += current;
         }
 
-        long long new_current = next%10;
-        next = next%10 + current%10;
-        current = new_current%10;
+        long long new_current = next;
+        next = next + current;
+        current = new_current;
     }
 
     return sum % 10;
 }
 
+long long find_sequence(long long n, long long m) {
+	long long  f_0 = 0, f_1 =1, f = f_0+f_1;
+    for(int i=0;i<m*m;i++)
+    {
+    	f = (f_1+f_0)%m;
+    	f_0 = f_1;
+    	f_1 = f;
+    	if(f_0==0 && f_1==1)
+    	{
+    		// std::cout<<"i = "<<i-1<<std::endl;
+    		return i+1;
+    	}
+    }
+}
+
+long long get_fibonacci_huge(long long n, long long m)
+{	
+	long long rem = n%find_sequence(n,m);
+	long long f_0 = 0, f_1 = 1, f = rem;
+	for(int i=1;i<rem;i++)
+	{
+		f = (f_1+f_0)%m;
+    	f_0 = f_1;
+    	f_1 = f;
+	}
+    if((f%m)==0)
+        return 9;
+    else
+	    return (f%m)-1;
+}
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    if(from==0 && to==0)
+        std::cout <<0<< '\n';        
+    else if(from==to)
+        std::cout <<get_fibonacci_huge(to, 10)+1<< '\n';
+    else
+        {
+            long long from_sum =get_fibonacci_huge(from+1, 10);
+            long long to_sum =  get_fibonacci_huge(to+2, 10);
+            //std::cout<<"from_sum = "<<from_sum<<std::endl;
+            //std::cout<<"to_sum = "<<to_sum<<std::endl;
+            if(to_sum-from_sum < 0)
+                std::cout<<(to_sum-from_sum+10)<<std::endl;
+            else
+                std::cout<<(to_sum-from_sum)<<std::endl;
+            //std::cout<<get_fibonacci_huge(to+2, 10)<<"\t"<<get_fibonacci_huge(from+2, 10)<<std::endl;
+            //std::cout << get_fibonacci_huge(to+2, 10)-get_fibonacci_huge(from+2, 10) << '\n';
+        }
 }
 
---
+++ Algorithmic_Toolbox/2_week/8_fibonacci_sum_squares.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+int fibonacci_sum_squares_naive(long long n) {
+    if (n <= 1)
+        return n;
+
+    long long previous = 0;
+    long long current  = 1;
+    long long sum      = 1;
+
+    for (long long i = 0; i < n - 1; ++i) {
+        long long tmp_previous = previous;
+        previous = current;
+        current = tmp_previous + current;
+        sum += current * current;
+    }
+
+    return sum % 10;
+}
+
+long long find_sequence(long long n, long long m) {
+	long long  f_0 = 0, f_1 =1, f = f_0+f_1;
+    for(int i=0;i<m*m;i++)
+    {
+    	f = (f_1+f_0)%m;
+    	f_0 = f_1;
+    	f_1 = f;
+    	if(f_0==0 && f_1==1)
+    	{
+    		// std::cout<<"i = "<<i-1<<std::endl;
+    		return i+1;
+    	}
+    }
+}
+
+long long get_fibonacci_huge(long long n, long long m)
+{	
+	long long rem = n%find_sequence(n,m);
+	long long f_0 = 0, f_1 = 1, f = rem;
+	for(int i=1;i<rem;i++)
+	{
+		f = ((f_1*f_1)+(f_0*f_0))%m;
+        //f = (f*f)%m;
+    	f_0 = f_1;
+    	f_1 = f;
+	}
+    if((f%m)==0)
+        return 9;
+    else
+	    return (f%m)-1;
+}
+int main() {
+    long long n = 0;
+    std::cin >> n;
+    std::cout << get_fibonacci_huge(n+2,10);
+}
+
